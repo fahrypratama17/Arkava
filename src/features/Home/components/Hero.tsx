af4bc12f213d5b480d7dd68531fd9cbc0249acdd
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-06-16 03:05:15 +0700

    feat: add Hero and HomeContainer components for homepage structure

---
+++ src/features/Home/components/Hero.tsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <section className="min-h-screen">
+      <h1>
+        Kuasai Web Development <span>dari Nol</span>
+      </h1>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/features/Home/container/HomeContainer.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/features/Home/components/Hero";
+
+const HomeContainer = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+};
+
+export default HomeContainer;
