7e8572731744014e991e0073c776284f497bee8e
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-09-19 12:56:26 +0700

    feat: update Hero section with new text content, button styling, and background color for improved visibility

--- src/features/Home/components/Hero.tsx
+++ src/features/Home/components/Hero.tsx
@@ -1,23 +1,25 @@
 import Image from "next/image";
+import { ArrowRight } from "lucide-react";
 
 const Hero = () => {
   return (
     <section className="grid min-h-screen grid-cols-2 items-center justify-center">
       <div className="space-y-8">
-        <h1 className="font-jakarta-700 text-5xl leading-[1.15]">
+        <h1 className="font-jakarta-700 text-5xl leading-[1.15] text-white">
           Bangun Karier Tech Impian:{" "}
-          <span className="bg-linear-to-r from-blue-400 via-blue-500 to-blue-600 bg-clip-text text-transparent">
+          <span className="from-blue-450 via-blue-250 to-blue-350 bg-linear-to-r bg-clip-text text-transparent">
             <br />
             Dari Baris Kode Pertama Hingga Siap Kerja
           </span>
         </h1>
-        <p className="font-jakarta-400 w-[70%] text-base">
+        <p className="font-jakarta-400 w-[70%] text-base text-white">
           Pelajari fondasi HTML, keindahan CSS, dan kecanggihan JavaScript
           melalui kurikulum terarah, latihan koding langsung di browser, serta
           portofolio nyata yang dilirik rekruter.
         </p>
-        <button className="font-jakarta-500 bg-blue-550 w-full max-w-40 cursor-pointer rounded-[18px] py-3 text-base text-white">
-          Jelajahi Materi
+        <button className="font-jakarta-700 bg-blue-550 flex w-fit cursor-pointer items-center gap-2 rounded-[18px] px-4 py-3 text-base text-white">
+          Jelajahi Materi Sekarang{" "}
+          <ArrowRight size={24} className="size-4 font-bold lg:size-5" />
         </button>
       </div>
       <div>
